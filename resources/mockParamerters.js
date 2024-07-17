ab49ab72a8c5380bce5a679e2e7491216b4f6524
kusariya <yusaki.4kusari@gmail.com>
2024-07-17 11:47:47 +0900

    MapTickerに対しコンストラクタが作成できることろまで確認済

--- resources/mockParamerters.js
+++ resources/mockParamerters.js
@@ -1,7 +1,7 @@
 import {jest} from "@jest/globals";
 
 // 単体試験で頻繁に使用するオブジェクトをmock化してます
-
+// TODO:以下のパラメータはSVGMapLv0.1_rXXmoduleに含まれており、いずれ外だしが必要
 const mock_svgmapObj = {
     refreshScreen: jest.fn(), 
     getSvgImagesProps: jest.fn().mockReturnValue({
@@ -9,7 +9,8 @@ const mock_svgmapObj = {
     }),
     getSvgImages: jest.fn(),
     getRootLayersProps: jest.fn().mockReturnValue([]),
-    getGeoViewBox: jest.fn().mockReturnValue({"x":0,"y":0,"width":0,"height":0})
+    getGeoViewBox: jest.fn().mockReturnValue({"x":0,"y":0,"width":0,"height":0}),
+    getMapCanvasSize: jest.fn().mockReturnValue({width: 1200, height: 800})
 };
 
 const mock_mapViewerProps = {
