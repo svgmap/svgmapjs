ee5244fc6070fcbba2e092627b508ec488bb883f
kusariya <yusaki.4kusari@gmail.com>
2026-02-27 12:04:34 +0900

    Add license information to regression test file

--- tests/e2e/regression.spec.ts
+++ tests/e2e/regression.spec.ts
@@ -1,3 +1,8 @@
+// License: (MPL v2)
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import { test, expect, BrowserContext, Page } from '@playwright/test';
 import path from 'path';
 import fs from 'fs';
